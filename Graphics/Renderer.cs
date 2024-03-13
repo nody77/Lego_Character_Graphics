89ca0b53819b1892b7595e3cf0754b844ecdb0c6
Nada <104033293+nody77@users.noreply.github.com>
2024-03-13 13:12:12 +0200

    drawing the right arm and right shirt

--- Graphics/Renderer.cs
+++ Graphics/Renderer.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+    Nada Ahmed Fathalla
+    2021170580
+    CS
+    Section 7
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,35 +19,85 @@ namespace Graphics
     {
         Shader sh;
         uint vertexBufferID;
+        
         public void Initialize()
         {
             string projectPath = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
             sh = new Shader(projectPath + "\\Shaders\\SimpleVertexShader.vertexshader",
                 projectPath + "\\Shaders\\SimpleFragmentShader.fragmentshader");
+            
             Gl.glClearColor(0.5f, 0.87f, 0.91f, 1);
             
             float[] points =
             {
-                0.7f , 0.1f , 0 , //right up corner
-                0.7f , -0.1f , 0 , //right down corner
-                -0.3f , 0.1f , 0 , // left up corner
-                -0.5f , 0.1f , 0 ,
-                0.5f , -0.1f , 0 ,
-                -0.5f, -0.1f, 0 
+                // right hand of shirt
+               0.6f , 0.29f , 0.0f, 
+               0.0f , 0.611f , 0.0392f,
+               0.51f , 0.29f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.51f , 0.06f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.51f , 0.06f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.6f , 0.06f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.6f , 0.29f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+
+               //right hand
+               0.6f,0.06f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.51f,0.06f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.6f,-0.17f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.51f,-0.17f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.6f,-0.17f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.51f,0.06f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+
+               //right shirt
+               0.51f , 0.38f,0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.31f , 0.38f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.31f, -0.055f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.31f, -0.055f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.51f, -0.055f , 0.0f,
+               0.0f , 0.611f , 0.0392f,
+               0.51f , 0.38f,0.0f,
+               0.0f , 0.611f , 0.0392f,
             };
+
+            
+
             vertexBufferID = GPU.GenerateBuffer(points);
+           
         }
 
         public void Draw()
         {
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
             sh.UseShader();
-            //
-            Gl.glEnableVertexAttribArray(0);
-            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, (sizeof(float))*3, IntPtr.Zero);
-            //
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 3);
             
+            Gl.glEnableVertexAttribArray(0);
+            Gl.glEnableVertexAttribArray(1);
+
+            Gl.glVertexAttribPointer(0, 3, Gl.GL_FLOAT, Gl.GL_FALSE, sizeof(float) * 6, IntPtr.Zero);
+            Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, sizeof(float) * 6, (IntPtr)(sizeof(float) * 3));
+            
+            //right arm
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 6, 6);
+
+            //right shirt
+            Gl.glDrawArrays(Gl.GL_TRIANGLES,12 , 6);
+
+            Gl.glDisableVertexAttribArray(1);
             Gl.glDisableVertexAttribArray(0);
         }
         public void Update()
