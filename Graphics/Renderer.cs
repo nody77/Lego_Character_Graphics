a0ed3972368c33e35d2de5ebc5b70d2b79d69cf0
Nada <104033293+nody77@users.noreply.github.com>
2024-03-13 13:45:00 +0200

    drawing the top and the chest

--- Graphics/Renderer.cs
+++ Graphics/Renderer.cs
@@ -31,46 +31,77 @@ namespace Graphics
             float[] points =
             {
                 // right hand of shirt
-               0.6f , 0.29f , 0.0f, 
+               0.54f , 0.29f , 0.0f, 
                0.0f , 0.611f , 0.0392f,
-               0.51f , 0.29f , 0.0f,
+               0.45f , 0.29f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.51f , 0.06f , 0.0f,
+               0.45f , 0.06f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.51f , 0.06f , 0.0f,
+               0.45f , 0.06f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.6f , 0.06f , 0.0f,
+               0.54f , 0.06f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.6f , 0.29f , 0.0f,
+               0.54f , 0.29f , 0.0f,
                0.0f , 0.611f , 0.0392f,
 
                //right hand
-               0.6f,0.06f,0.0f,
+               0.54f,0.06f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
-               0.51f,0.06f,0.0f,
+               0.45f,0.06f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
-               0.6f,-0.17f,0.0f,
+               0.54f,-0.17f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
-               0.51f,-0.17f,0.0f,
+               0.45f,-0.17f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
-               0.6f,-0.17f,0.0f,
+               0.54f,-0.17f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
-               0.51f,0.06f,0.0f,
+               0.45f,0.06f,0.0f,
                255f/255f, 204f/255f, 115f/255f,
 
                //right shirt
-               0.51f , 0.38f,0.0f,
+               0.45f , 0.38f,0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.31f , 0.38f , 0.0f,
+               0.25f , 0.38f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.31f, -0.055f , 0.0f,
+               0.25f, -0.055f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.31f, -0.055f , 0.0f,
+               0.25f, -0.055f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.51f, -0.055f , 0.0f,
+               0.45f, -0.055f , 0.0f,
                0.0f , 0.611f , 0.0392f,
-               0.51f , 0.38f,0.0f,
+               0.45f , 0.38f,0.0f,
                0.0f , 0.611f , 0.0392f,
+
+               //upper chest
+               0.25f , 0.38f , 0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.09f , 0.38f, 0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.09f,0.29f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.09f,0.29f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.25f,0.29f,0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+               0.25f , 0.38f , 0.0f,
+               255f/255f, 204f/255f, 115f/255f,
+
+               //top
+               0.25f,0.29f,0.0f,
+               255f, 255f, 255f,
+               0.09f,0.29f,0.0f,
+               255f, 255f, 255f,
+               0.09f, -0.055f , 0.0f,
+               255f, 255f, 255f,
+               0.09f, -0.055f , 0.0f,
+               255f, 255f, 255f,
+               0.25f,-0.055f,0.0f,
+               255f, 255f, 255f,
+               0.25f,0.29f,0.0f,
+               255f, 255f, 255f,
+
+               //Left Shirt
+
             };
 
             
@@ -91,11 +122,17 @@ namespace Graphics
             Gl.glVertexAttribPointer(1, 3, Gl.GL_FLOAT, Gl.GL_FALSE, sizeof(float) * 6, (IntPtr)(sizeof(float) * 3));
             
             //right arm
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0, 6);
-            Gl.glDrawArrays(Gl.GL_TRIANGLES, 6, 6);
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 0 , 6);
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 6 , 6);
 
             //right shirt
-            Gl.glDrawArrays(Gl.GL_TRIANGLES,12 , 6);
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 12 , 6);
+
+            //upper chest
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 18 , 6);
+
+            //Top
+            Gl.glDrawArrays(Gl.GL_TRIANGLES, 24, 6);
 
             Gl.glDisableVertexAttribArray(1);
             Gl.glDisableVertexAttribArray(0);
